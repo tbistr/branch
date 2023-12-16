eb4b25b2de36d7d2e9eda04393a25d5f19f1f6ce
tbistr <tbistr99@gmail.com>
2023-12-16 15:02:07 +0000

    feat: Add a command parameter for grep.

--- cmd/root.go
+++ cmd/root.go
@@ -11,6 +11,11 @@ import (
 	"github.com/tbistr/branch/view"
 )
 
+var (
+	// Used for flags.
+	greps []string
+)
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "branch",
@@ -23,60 +28,7 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {
-
-		filters := []string{"python", "x86", "ssh"}
-
-		type grepper struct {
-			filter string
-			c      chan string
-		}
-
-		greppers := make([]grepper, 0)
-
-		for _, text := range filters {
-			c := make(chan string)
-			greppers = append(greppers, grepper{
-				filter: text,
-				c:      c,
-			})
-		}
-		defaultC := make(chan string)
-
-		stdinScanner := bufio.NewScanner(os.Stdin)
-		go func() {
-			for stdinScanner.Scan() {
-				line := stdinScanner.Text()
-				shouldDefault := true
-				for _, g := range greppers {
-					if strings.Contains(line, g.filter) {
-						g.c <- stdinScanner.Text()
-						shouldDefault = false
-					}
-				}
-				if shouldDefault {
-					defaultC <- stdinScanner.Text()
-				}
-			}
-		}()
-
-		cs := make([]<-chan string, 0)
-		for _, grepper := range greppers {
-			cs = append(cs, grepper.c)
-		}
-		cs = append(cs, defaultC)
-
-		p := tea.NewProgram(
-			view.New(cs),
-			tea.WithAltScreen(), // use the full size of the terminal in its "alternate screen buffer"
-			// tea.WithMouseCellMotion(), // turn on mouse support so we can track the mouse wheel
-		)
-
-		if _, err := p.Run(); err != nil {
-			fmt.Println("could not run program:", err)
-			os.Exit(1)
-		}
-	},
+	Run: rootCmdRun,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -89,13 +41,57 @@ func Execute() {
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	rootCmd.Flags().StringArrayVar(&greps, "grep", []string{}, "Filters to apply to the output")
+	rootCmd.MarkFlagRequired("grep")
+}
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.branch.yaml)")
+func rootCmdRun(cmd *cobra.Command, args []string) {
+	type grepper struct {
+		filter string
+		c      chan string
+	}
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	greppers := make([]grepper, 0)
+	for _, text := range greps {
+		c := make(chan string)
+		greppers = append(greppers, grepper{
+			filter: text,
+			c:      c,
+		})
+	}
+	defaultC := make(chan string)
+
+	stdinScanner := bufio.NewScanner(os.Stdin)
+	go func() {
+		for stdinScanner.Scan() {
+			line := stdinScanner.Text()
+			shouldDefault := true
+			for _, g := range greppers {
+				if strings.Contains(line, g.filter) {
+					g.c <- stdinScanner.Text()
+					shouldDefault = false
+				}
+			}
+			if shouldDefault {
+				defaultC <- stdinScanner.Text()
+			}
+		}
+	}()
+
+	cs := make([]<-chan string, 0)
+	for _, grepper := range greppers {
+		cs = append(cs, grepper.c)
+	}
+	cs = append(cs, defaultC)
+
+	p := tea.NewProgram(
+		view.New(cs),
+		tea.WithAltScreen(), // use the full size of the terminal in its "alternate screen buffer"
+		// tea.WithMouseCellMotion(), // turn on mouse support so we can track the mouse wheel
+	)
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("could not run program:", err)
+		os.Exit(1)
+	}
 }
